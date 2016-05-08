95e5b45a2c1b5702f70e0ae74fbc0cfbf6221f9e
Irina Lavryonova <lavryonovairina@gmail.com>
2016-05-08 04:00:19 -0700

    started Pot development

--- lib/Pot/Pot.cpp
+++ lib/Pot/Pot.cpp
@@ -1,31 +1,26 @@
-#include <Wire.h>
-#include <Adafruit_GFX.h>
 #include <Adafruit_IS31FL3731.h>
 #include <Arduino.h>
 #include <string.h>
 #include <Pot.h>
+#define ARRAY_SIZE(array) (sizeof((array))/sizeof((array[0])))
 
 
-Pot::Pot(int pin){
+Pot::Pot(int pin, Adafruit_IS31FL3731* display){
+  this->display = display; // TODO how to fix this?
   this->pin = pin;
-  printer = Printer();
-
-  isFirstCalDone = false;
-  isSecondCalDone = false;
-  isTwoCalDone = false;
 }
 
-void Pot::calibrate(Adafruit_IS31FL3731 displays[], uint8_t totalNumOfPots, int calButtonPin){
-  printer.printText(5, 0, "Calibration ", 1, 1, displays, 1000, 5);
-
+void Pot::calibrate(uint8_t totalNumOfPots, int calButtonPin){
   for(int i = 0; i < totalNumOfPots; i++){
       //Calibrate one of the pots
       //Touch top of the pot and record the value. Make sure to sample for a little bit to get an accurate value.
       //Draw instructions for the user
-      printer.printText(5, 0, "Press and hold the top of one pot", 1, 1, displays, 1000, 1);
-      this->max = sampleValue(displays);
-      printer.printText(5, 0, "Press and hold the bottom of one pot", 1, 1, displays, 1000, 1);
-      this->min = sampleValue(displays);
+      do{
+        display.printText(0, 0, "Press and hold the top of one pot", 1, true, true, 0, 5);
+        this->max = sampleValue();
+        display.printText(0, 0, "Press and hold the bottom of one pot", 1, true, true, 0, 5);
+        this->min = sampleValue();
+      } while(false);
       //Touch bottom of the pot and record the value. Make sure to sample for a little bit to get an accurate value.
       //Draw instructions for the user
       /**/
@@ -34,14 +29,14 @@ void Pot::calibrate(Adafruit_IS31FL3731 displays[], uint8_t totalNumOfPots, int 
   }
 }
 
-double Pot::sampleValue(Adafruit_IS31FL3731 displays[]){
+double Pot::sampleValue(){
   delay(2000); //2 secs
-  printer.printText(5, 0, "Sample start", 1, 1, displays, 0, 1);
+  display.printText(0, 0, "Sample Start", 1, true, true, 0, 5);
   double sum = 0;
   for(int i = 0; i < 50; i++){
     sum += analogRead(pin);
   }
-  printer.printText(5, 0, "OK!", 1, 1, displays, 0, 1);
+  display.printText(0, 0, "OK!", 1, true, true, 0, 5);
   return sum/50;
 }
 
@@ -51,4 +46,23 @@ double Pot::getNormalizedRead(){
 
 int Pot::getLEDNumRead(){
   return 15*getNormalizedRead();
+}
+
+bool Pot::test(){
+  display.printText(0, 0,
+    "Run your finger all alone the pot and check that the led light up all alone the bottom row",
+    1, true, true, 1, 5);
+  while(/*time for this test*/ false){
+    display.setLEDPWM(getLEDNumRead(), 1);
+  }
+
+  display.printText(0, 0,
+    "Press the button if satisfied, don't do anything is not",
+    1, true, true, 1, 4);
+
+  if(digitalRead(pin)){
+    return true;
+  } else{
+    return false;
+  }
 }
--- lib/Pot/Pot.h
+++ lib/Pot/Pot.h
@@ -1,25 +1,22 @@
 #pragma once
-#include <Wire.h>
-#include <Printer.h>
+#include <Arduino.h>
+#include <Adafruit_IS31FL3731.h>
 
 class Adafruit_IS31FL3731;
 
 class Pot{
 public:
-  Pot(int pin);
-  void calibrate(Adafruit_IS31FL3731 displays[], uint8_t totalNumOfPots, int calButtonPin);
+  Pot(int pin, Adafruit_IS31FL3731* display);
+  void calibrate(uint8_t totalNumOfPots, int calButtonPin);
   int getLEDNumRead(void);
 private:
   int pin;
   int max;
   int min;
 
-  Printer printer;
+  Adafruit_IS31FL3731 display;
 
-  bool isFirstCalDone;
-  bool isSecondCalDone;
-  bool isTwoCalDone;
-
-  double sampleValue(Adafruit_IS31FL3731 displays[]);
+  double sampleValue(void);
   double getNormalizedRead(void);
+  bool test(void);
 };
