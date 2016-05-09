794d30c32a4c21517131429ff517bdcbb2aa5dda
Irina Lavryonova <lavryonovairina@gmail.com>
2016-05-08 22:03:59 -0700

    updated with hardware lib

---
+++ mains/PotCalTest.cpp
@@ -0,0 +1,42 @@
+#include <Adafruit_IS31FL3731.h>
+#include <Arduino.h>
+#include <string.h>
+#include <Pot.h>
+#include <Hardware.h>
+
+Adafruit_IS31FL3731 ledmatrix74 = Adafruit_IS31FL3731();
+Adafruit_IS31FL3731 ledmatrix75 = Adafruit_IS31FL3731();
+
+Hardware hardware = Hardware();
+
+Pot pots[] = {Pot(hardware.dPin74, &ledmatrix74), Pot(hardware.dPin75, &ledmatrix75)};
+
+uint8_t calButtonPin = 7;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("On/Off");
+
+  if(!ledmatrix74.begin(hardware.address74) || !ledmatrix75.begin(hardware.address75)){
+    Serial.println("Error! Display not found!");
+    while(1);
+  }
+  Serial.println("Display found");
+  ledmatrix74.setRotation(2);
+
+  // pots[0].calibrate(2, calButtonPin);
+  // pots[1].calibrate(2, calButtonPin);
+
+}
+
+void loop() {
+  double lednum0 = pots[0].getLEDNumRead();
+  double lednum5 = pots[1].getLEDNumRead();
+
+  Serial.println("Pin 0: " + String(lednum0) + " Pin 5: " + String(lednum5));
+
+  ledmatrix74.setLEDPWM(lednum0, 20, 0);
+  ledmatrix74.clear();
+  ledmatrix75.setLEDPWM(lednum5, 20, 0);
+  ledmatrix75.clear();
+}
--- mains/PrinterTest.cpp
+++ mains/PrinterTest.cpp
@@ -1,9 +1,8 @@
-#include <Wire.h>
-#include <Adafruit_GFX.h>
 #include <Adafruit_IS31FL3731.h>
 #include <Arduino.h>
 #include <string.h>
 #include <ErrorCodes.h>
+#include <Hardware.h>
 
 Adafruit_IS31FL3731 ledmatrix74 = Adafruit_IS31FL3731();
 Adafruit_IS31FL3731 ledmatrix75 = Adafruit_IS31FL3731();
@@ -15,13 +14,13 @@ ErrorCodes errorCodes = ErrorCodes();
 bool print = true;
 String testString = "TESTING, testing. 123!@$";
 
-//Printer printer = Printer();
+Hardware hardware = Hardware();
 
 void setup() {
   Serial.begin(9600);
   Serial.println("On/Off");
 
-  if(!ledmatrix74.begin(0x74) || !ledmatrix75.begin(0x75)){
+  if(!ledmatrix74.begin(hardware.address74) || !ledmatrix75.begin(hardware.address75)){
     errorCodes.writeErrorCode(errorCodes.DISP_NOT_FOUND);
     //Serial.println("Error! Display not found!");
     while(1);
