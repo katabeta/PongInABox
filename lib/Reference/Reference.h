59129816820c16c7a21529a90c0ebee5068c2906
Irina Lavryonova <lavryonovairina@gmail.com>
2016-05-09 23:45:04 -0700

    renamed Hardware to Reference

---
+++ lib/Reference/Reference.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <Arduino.h>
+
+
+
+class Reference{
+public:
+  Reference(void);
+  uint8_t dPin74 = 2;
+  uint8_t dPin75 = 3;
+  uint8_t dPin0 = 4;
+
+  uint8_t aPin74 = 0;
+  uint8_t aPin75 = 1;
+
+  uint8_t address74 = 0x74;
+  uint8_t address75 = 0x75;
+
+  uint16_t potMin = 375;
+  uint16_t potMax = 710;
+};
