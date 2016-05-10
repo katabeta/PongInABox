359b1c49dea9c4bf959ec55acf880a391c802919
Irina Lavryonova <lavryonovairina@gmail.com>
2016-05-09 23:43:32 -0700

    Removed unnecessary Pot methods

--- lib/Pot/Pot.cpp
+++ lib/Pot/Pot.cpp
@@ -11,17 +11,6 @@ Pot::Pot(int pin, Adafruit_IS31FL3731* display){
 }
 
 /*public*/
-// void Pot::calibrate(uint8_t totalNumOfPots){
-//   for(int i = 0; i < totalNumOfPots; i++){
-//       do{
-//         display->printText(0, 0, "Press and hold the top of your control surface", 1, true, true, 1, 5);
-//         this->max = sampleValue();
-//         display->printText(0, 0, "Press and hold the bottom of your control surface", 1, true, true, 1, 5);
-//         this->min = sampleValue();
-//       } while(calTest());
-//   }
-// }
-
 int Pot::getLEDNumRead(){
   int ledNum = 15 * getNormalizedRead();
   return ledNum > 15 ? -1 : ledNum;
@@ -31,58 +20,26 @@ int Pot::getLEDNumRead(double min, double max){
   return ledNum > 15 ? -1 : ledNum;
 }
 
+//TODO untested
+bool Pot::test(){
+  display->printText(0, 0, "Test", 10, true, false, 0, 5);
+  delay(500);
+  display->printText(0, 0, "Press button when done", 10, true, true, 2, 5);
+  delay(500);
+  bool done = false; //TODO can remove and return true
+  while(!done){
+    display->setLEDPWM(getLEDNumRead(), 254);
+    done = digitalRead(pin);
+  }
+  return done;
+}
+
 
 
 /*private*/
-// bool Pot::syncCal(Pot *pots){
-//
-// }
-
-// double Pot::sampleValue(){
-//   delay(2000); //2 secs
-//   display->printText(5, 0, "3", 1, true, false, 0, 5);
-//   delay(500);
-//   display->printText(5, 0, "2", 1, true, false, 0, 5);
-//   delay(500);
-//   display->printText(5, 0, "1", 1, true, false, 0, 5);
-//   delay(500);
-//   display->printText(5, 0, "0", 1, true, false, 0, 5);
-//   delay(500);
-//   display->printText(0, 0, "Queing", 1, true, false, 0, 5);
-//   double sum = 0;
-//   for(int i = 0; i < 50; i++){
-//     sum += analogRead(pin);
-//   }
-//   display->printText(5, 0, "OK!", 1, true, false, 0, 5);
-//   return sum/50;
-// }
-
 double Pot::getNormalizedRead(){
-  return (analogRead(pin)-hardware.potMin)/(hardware.potMax-hardware.potMin);
+  return (analogRead(pin)-reference.potMin)/(reference.potMax-reference.potMin);
 }
 double Pot::getNormalizedRead(double min, double max){
-  double asd=(analogRead(pin)-min)/(max-min);
-  return asd;
+  return (analogRead(pin)-min)/(max-min);
 }
-
-// bool Pot::calTest(){
-//   bool doneTesting = false;
-//   display->printText(0, 0,
-//     "Run your finger all along the pot and check the LEDs the light up match your movement",
-//     1, true, true, 1, 1);
-//
-//     //TODO may want to display this message at the same time as testing
-//   display->printText(0, 5,
-//     "Press the button when you are done",
-//     1, true, true, 1, 5);
-//   while(!doneTesting){
-//     display->setLEDPWM(getLEDNumRead(), 1);
-//     doneTesting = digitalRead(pin);
-//   }
-//
-//   display->printText(0, 0,
-//     "Press the button if disatisfied, else nothing",
-//     1, true, true, 1, 5);
-//
-//   return digitalRead(pin) ? true : false;
-// }
--- lib/Pot/Pot.h
+++ lib/Pot/Pot.h
@@ -1,22 +1,22 @@
 #pragma once
 #include <Arduino.h>
 #include <Adafruit_IS31FL3731.h>
-#include <Hardware.h>
+#include <Reference.h>
 
 class Adafruit_IS31FL3731;
 
 class Pot{
 public:
-  Pot(int pin, Adafruit_IS31FL3731* display);
-  //void calibrate(uint8_t totalNumOfPots);
+  Pot(uint8_t pin, Adafruit_IS31FL3731* display);
   int getLEDNumRead(void);
   int getLEDNumRead(double min, double max);
+  bool test(void);
 private:
   int pin;
   int max;
   int min;
 
-  Hardware hardware = Hardware();
+  Reference reference = Reference();
 
   //bool syncCal(Pot pots[]);
 
