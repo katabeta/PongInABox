88c8bd9e60afee7394fd7660121f540d3df3ba9e
Irina Lavryonova <lavryonovairina@gmail.com>
2016-05-12 01:24:26 -0700

    updated todolist

--- lib/Reference/Reference.cpp
+++ lib/Reference/Reference.cpp
@@ -3,5 +3,7 @@
 /**
 @class Reference
     contains constant variables.
+
+@todo add #include <___.h> documentation to the mainpage to exaplain how to integrate everything into a main.cpp
 */
 Reference::Reference(void){}
